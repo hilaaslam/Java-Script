01228da6a41c0ef48fba3de4e97cb63873a8183b
HILA_ASLAM <hila@HILA_ASLAM.com>
2023-04-07 12:52:35 +0500

    FEAt:JS-intro

---
+++ script/app.js
@@ -0,0 +1,52 @@
+                        //    DAY 1 APRIL-6
+
+
+// alert("welcome")
+// var user = prompt("Your Name PLease","Hila")
+
+// let arr = ('i','am','here')
+// document.write(arr)
+// console.log(arr)
+
+// var imAstudent; //camelCase
+// var ImAStudent; // PascalCase
+// var im_a_student //snake_case
+
+// alert(imAstudent)
+// var num = prompt("write any number")
+// console.log(num)
+
+// let num1 = +prompt("Write Number 1 :")
+// let num2 = +prompt("Write Number 2 :")
+// console.log("Number i:"+(num1) + " Number ii:"+(num2) +" = "+ (num1+num2))
+// console.log(num1+num2)
+// console.log("1"+"4")
+
+// console.log(2+4)
+// console.log(2-4)
+// console.log(2*4)
+// console.log(2/4)
+// console.log(2**4)
+// console.log(6+4)
+
+// console.log(((1*(30-26))*(32/12)*4)+(8*2)/1)
+// var name = "I Am"
+// var fname = "Hila"
+// console.log(name+ " " + fname);
+
+// var num = 2
+// console.log(num)
+// num = num+10
+// num *=10
+// num++
+// ++num
+// num--
+// console.log(num+"6")
+
+                    //    DAY 2 APRIL-7
+
+  var zakatPercentage = 0.025;
+  var YourAmount = +prompt("Enter Your Amount");
+  var result = zakatPercentage * YourAmount ;
+  alert("Your Zakat Value is " + result);
+  console.log("Your Zakat Value is " + result);
